1ea02dbcd307b35094b84ad546ab1952c2f7153a
Chris Lane <chlane@paypal.com>
2016-02-26 11:49:44 -0800

    update

--- rally.py
+++ rally.py
@@ -29,54 +29,60 @@ import my_config
 project = my_config.project()
 todo_story = my_config.live_support()
 
+def dict_from_story(story):
+    details = {}
+    for a_key in story.attributes():
+        details[a_key] = str(story.__dict__[a_key])
+    return details
+    
 
-def make_story(story_name, story_detail):
+def make_story(story_name, story_detail, username):
     r = Rally(server, apikey=apikey, project=project)
     r.enableLogging('rally.log')
     wksp = r.getWorkspace()
     proj = r.getProject('DG-Monitoring')
     info = {"Workspace":   wksp.ref,
             "Project":     proj.ref,
-            "Name":        story_name + " (web intake form)",
+            "Name":        story_name + " (web intake form from " + username + ")",
             "Story Type":  "New Feature",
             "Description": story_detail}
 
     story = r.put('Story', info)
-    return story.details()
+    return dict_from_story(story)
 
+
+def get_story(formatted_id):
+    r = Rally(server, apikey=apikey, project=project)
+    r.enableLogging('rally.log')
+    aa = r.get('UserStory',
+               fetch=True,
+               query='FormattedID = "' + formatted_id + '"',
+               instance=True)
+    return dict_from_story(aa)
 
 def make_task(task_name, task_user, task_detail, task_hours):
-
     r = Rally(server, apikey=apikey, project=project)
-
     wksp = r.getWorkspace()
     proj = r.getProject('DG-Monitoring')
 
-    aa = r.get('UserStory', fetch='FormattedID', query='FormattedID = "' + todo_story + '"')
-    for story in aa:
-        info = {"Workspace":   wksp.ref,
-                "Project":     proj.ref,
-                "WorkProduct": story.ref,
-                "Name":        task_name + " (" + task_user + ")",
-                "State":       "Completed",
-                "Estimate":    hours,
-                "Actuals":   hours,
-                "TaskIndex":   1,
-                "Description": task_detail + " autocreated."}
+    aa = r.get('UserStory', fetch='FormattedID', query='FormattedID = "' + todo_story + '"', instance=True)
+    info = {"Workspace":   wksp.ref,
+            "Project":     proj.ref,
+            "WorkProduct": aa.ref,
+            "Name":        task_name + " (" + task_user + ")",
+            "State":       "Completed",
+            "Estimate":    hours,
+            "Actuals":     hours,
+            "TaskIndex":   1,
+            "Description": task_detail + " autocreated."}
 
-        task = r.put('Task', info)
-        for b in task:
-            return b.details()
+    task = r.put('Task', info)
+    for b in task:
+        return b.details()
 
 
 if __name__ == '__main__':
-    r = Rally(server, apikey=apikey, project=project)
-
-    wksp = r.getWorkspace()
-    proj = r.getProject()
-    response = r.get('UserStory', fetch=True, query='FormattedID = "US542314"')
-    story1 = response.next()
-    print story1.details()
+    print get_story('US560419')
     os._exit()
     import getpass
     hours = raw_input('How many hours: ')
